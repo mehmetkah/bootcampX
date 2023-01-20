030adeadffcfa888f995c2a2ba0faa1cbdd8045e
“mehmetkah” <mehmetkahca@gmail.com>
2023-01-20 01:30:35 -0700

    git bug fixed

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "labber",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+const cohort = process.argv[2];
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`;
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((row) => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  });
+});
